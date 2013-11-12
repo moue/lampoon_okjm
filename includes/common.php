741cd96d4a5999f82b81a477f7345fa42a2327c9
moue <hou@college.harvard.edu>
2013-11-11 23:44:20 -0500

    Update common.php
    removed passwords

--- includes/common.php
+++ includes/common.php
@@ -1,11 +1,7 @@
 <?php
 	session_start();
 		
-	//define database constants
-	define("DB_USER","podcast");
-	define("DB_PASS","G5FJYPXUiVoe");
-	define("DB_HOST","mysql.hcs.harvard.edu");
-	define("DB_NAME","podcast");
+	// see flashdrive for db connection pw info
 
 	//create a DB connection
 	$link = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die('Could not connect to mysql server.');
